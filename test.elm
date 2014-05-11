ecb3872c631b620e6c42b7cc73760a4775d76e31
Neil Vallon <neilvallon@gmail.com>
2014-05-11 05:34:22 -0500

    Add epsilon value to prevent smoother from calculating small numbers

--- test.elm
+++ test.elm
@@ -5,7 +5,7 @@ main = draw <~ Window.dimensions ~ circleSize
 
 draw (x, y) cs = collage x y [filled (rgba 0 0 0 1) (circle (0.5 * cs))]
 
-circleSize = smooth 0.9 (fps 25) velocity
+circleSize = dropRepeats <| smooth 0.9 (fps 25) 1 velocity
 
 velocity =
   let
@@ -16,9 +16,11 @@ velocity =
     moveVec = foldp (\n (a, b) -> (b, n)) ((0, 0), (0, 0)) delayPos
   in vecLen <~ (vecSub <~ moveVec)
 
-smooth factor interval sig = 
+smooth factor interval epsilon sig =
   let
-    tick = sampleOn interval sig
-    scale sn s = ((1.0 - factor) * sn) + (factor * s)
-    outp = foldp scale 0.0 tick
+    scale sn s =
+      let scaledVal = ((1.0 - factor) * sn) + (factor * s)
+      in if scaledVal < epsilon then 0 else scaledVal
+
+    outp = foldp scale 0.0 <| sampleOn interval sig
   in sampleOn interval outp

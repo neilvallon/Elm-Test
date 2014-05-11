6d8de4cf9e1b7aeb2df94a25f758b62688367f53
Neil Vallon <neilvallon@gmail.com>
2014-05-11 00:15:40 -0500

    Set circle size based on mouse velocity

--- test.elm
+++ test.elm
@@ -5,10 +5,16 @@ main = draw <~ Window.dimensions ~ circleSize
 
 draw (x, y) cs = collage x y [filled (rgba 0 0 0 1) (circle (0.5 * cs))]
 
-circleSize = smooth 0.9 (fps 25) mouseVector
+circleSize = smooth 0.9 (fps 25) velocity
 
-mouseVector = (\(x, y) -> toFloat x + toFloat y) <~ Mouse.position
+velocity =
+  let
+    vecSub ((x1, y1), (x2, y2)) = (x1 - x2, y1 - y2)
+    vecLen (x, y) = sqrt (toFloat x^2 + toFloat y^2)
 
+    delayPos = sampleOn (every (50 * millisecond)) Mouse.position
+    moveVec = foldp (\n (a, b) -> (b, n)) ((0, 0), (0, 0)) delayPos
+  in vecLen <~ (vecSub <~ moveVec)
 
 smooth factor interval sig = 
   let
